51e69420e4f14ddbe0689d55fb09fc6c47b3632e
Gaston82 <cabezaartaza@hotmail.com>
2023-02-24 12:18:02 +0100

    Add username,password and email validation

--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import loginUser, { registerUser } from "../controllers/usersControllers.js";
+import { check } from "express-validator";
 import multer from "multer";
 const upload = multer({ dest: "uploads/" });
 
 const usersRouter = Router();
 
 usersRouter.post("/login", loginUser);
-usersRouter.post("/register", upload.single("image"), registerUser);
+usersRouter.post(
+  "/register",
+  upload.single("image"),
+  [
+    check("username", "Username required").notEmpty(),
+    check("password", "Password is required").isLength({ min: 6 }),
+    check("email", "Email not valid").isEmail(),
+  ],
+  registerUser
+);
 
 export default usersRouter;
